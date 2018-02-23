db8c4d9579a2ca2599446e5ecb44e8c6a6843325
John Coggeshall <john@debian.coogle.local>
2018-02-23 00:02:15 -0500

    Updating the code base to use the AccelStepper library, and upgraded the hardware to use a significantly more powerful NEMA-17 stepper using the EasyDriver stepper control board.
    Updated the STLs as well and tweaked them to fine-tune the motion
    of the pump to hopefully ensure it doesn't freeze up or get stuck again.

--- coogle-doser.ino
+++ coogle-doser.ino
@@ -1,12 +1,16 @@
 #include <CoogleIOT.h>
-#include <Stepper2.h>
+#include <AccelStepper.h>
 
 #ifndef SERIAL_BAUD
 #define SERIAL_BAUD 115200
 #endif
 
-#ifndef STEPPER_RPMS
-#define STEPPER_RPMS 15
+#ifndef STEPPER_SPEED
+#define STEPPER_SPEED 1000
+#endif
+
+#ifndef STEPPER_ACCEL
+#define STEPPER_ACCEL 500
 #endif
 
 #define ACTION_TOPIC "/peristalic/freshwater/1"
@@ -15,11 +19,10 @@
 CoogleIOT *iot;
 PubSubClient *mqtt;
 
+AccelStepper stepper(AccelStepper::DRIVER, D1, D2);
+
 char msg[150];
-
-int pinOuts[4] = { D1, D2, D5, D6 };
-
-Stepper2 dosingStepper(pinOuts);
+int turnsToExecute = 0;
 
 void setup() {
 
@@ -30,13 +33,14 @@ void setup() {
    
   iot->info("CoogleDoser Initializing...");
   iot->info("-=-=-=-=--=--=-=-=-=-=-=-=-=-=-=-=-=-");
-  iot->logPrintf(INFO, "Doser RPM: %d\n", STEPPER_RPMS);
   iot->logPrintf(INFO, "MQTT Action Topic: %s", ACTION_TOPIC);
   iot->logPrintf(INFO, "MQTT State Topic: %s", STATE_TOPIC);
   iot->info("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-");
 
-  dosingStepper.setSpeed(STEPPER_RPMS);
-  
+  stepper.setMaxSpeed(STEPPER_SPEED);
+  stepper.setAcceleration(STEPPER_ACCEL);
+  stepper.setSpeed(STEPPER_SPEED);
+    
   iot->info("");
 
   if(iot->mqttActive()) {
@@ -57,44 +61,27 @@ void setup() {
 void mqttCallback(char *topic, byte *payload, unsigned int length)
 { 
   String input((char *)payload);
-  int turnsToExecute = input.toInt();
+  
+  turnsToExecute = input.toInt();
   
   iot->logPrintf(DEBUG, "MQTT Callback Triggered. Topic: %s\n", topic);
   iot->logPrintf(DEBUG, "Turns to execute: %d", turnsToExecute);
 
-  if(turnsToExecute > 0) {
-    
-    iot->info("Executing clock-wise");
-    
-    dosingStepper.setDirection(0);
-
-    for(int i = turnsToExecute; i > 0; i--) {
-      mqtt->publish(STATE_TOPIC, String(i).c_str(), true);
-      yield();
-      iot->loop();
-      dosingStepper.turn();
-    }
-    
-  } else if(turnsToExecute < 0) {
-    
-    iot->info("Executing counter-clockwise");
-    dosingStepper.setDirection(1);
-    
-    for(int i = turnsToExecute; i < 0; i++) {
-      mqtt->publish(STATE_TOPIC, String(i).c_str(), true);
-      yield();
-      iot->loop();
-      dosingStepper.turn();
-    }
-        
-  } 
-
-  mqtt->publish(STATE_TOPIC, "0", true);
-  dosingStepper.stop();
-  
   iot->info("Action Complete!");
 }
 
 void loop() {
   iot->loop();
+  
+  if(stepper.distanceToGo() == 0) {
+    if(turnsToExecute > 0) {
+      stepper.moveTo(stepper.currentPosition() + (1600 * turnsToExecute));
+      turnsToExecute = 0;
+
+      //mqtt->publish(STATE_TOPIC, String(turnsToExecute).c_str(), true);
+       
+    }
+  }
+  
+  stepper.run();
 }
